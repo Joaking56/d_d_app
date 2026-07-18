2d039018a8f9d0e06db1814f7fa7c78b44b3b599
joaking <okos100@gmail.com>
2026-07-18 19:16:13 +0200

    Second Commit -- Status State Hide Register From

--- main.py
+++ main.py
@@ -7,29 +7,36 @@ database_handling.init_db()
 st.title("Dotti & Donát Esküvője")
 st.subheader("Vendéglista Regisztráció")
 
-with st.form("registration_form", clear_on_submit=True):
-    first_name = st.text_input("Add meg a vezetékneved", key="f_name").capitalize().strip()
-    second_name = st.text_input("Add meg a keresztneved", key="s_name").capitalize().strip()
-    guests = st.selectbox(
-        "Kérlek, jelöld be egyedül hányan jöttök:",
-        options=[1, 2],
-        index=0
-    )
+if "registered" not in st.session_state:
+    st.session_state.registered = False
 
-    submitted = st.form_submit_button("Regisztráció")
+if not st.session_state.registered:
+    with st.form("registration_form", clear_on_submit=True):
+        first_name = st.text_input("Add meg a vezetékneved", key="f_name").capitalize().strip()
+        second_name = st.text_input("Add meg a keresztneved", key="s_name").capitalize().strip()
+        guest_amount = st.selectbox(
+            "Kérlek, jelöld be egyedül hányan jöttök:",
+            options=[1, 2],
+            index=0
+        )
 
-    if submitted:
-        if first_name.strip() != "" and second_name.strip() != "":
-            database_handling.add_guest(second_name, first_name, guests)
-            st.success("Regisztráció sikeres!")
-        else:
-            st.error("Kérlek töltsd ki a név mezőt!")
+        submitted = st.form_submit_button("Regisztráció")
+
+        if submitted:
+            if first_name.strip() != "" and second_name.strip() != "":
+                database_handling.add_guest(second_name, first_name, guest_amount)
+                st.session_state.registered = True
+                st.rerun()
+            else:
+                st.error("Kérlek töltsd ki a név mezőt!")
+else:
+    st.success("Regisztráció sikeres! Köszönjük! 🎉")
+
 
 df = pandas_work.create_df()
 guest_count = pandas_work.summ_guests(df)
-guests = df["first_name"] + " " + df["second_name"]
-guest_list_html = "<br>".join(guests.tolist())
-
+guest_names = df["first_name"] + " " + df["second_name"]
+guest_list_html = "<br>".join(guest_names.tolist())
 
 st.markdown(
     f"""
@@ -40,7 +47,6 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-
 
 st.markdown(
     f"""
