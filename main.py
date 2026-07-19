f2117b7581cf97e5f0c6a68967b5e4d87f446b3b
joaking <okos100@gmail.com>
2026-07-19 20:22:51 +0200

    5.4 Commit -- fixing parameters

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def build_guest_line(row):
 
 
 database_handling.init_db()
-background_pic.set_background("background.jpg", opacity=0.4, size="50%")
+background_pic.set_background("background.jpg", opacity=0.8, size="35%")
 st.title("Dotti & Donát Esküvője")
 st.subheader("Vendéglista Regisztráció")
 
